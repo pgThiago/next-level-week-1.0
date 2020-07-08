efa7ab8ae8acfdf686e7908a187ba43123532de0
Thiago Silva <thiago25pro@gmail.com>
2020-07-08 07:26:28 -0300

    Translucent set to false in App.tsx

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,13 +1,11 @@
 import React from 'react';
-import { View, Text, StatusBar } from 'react-native';
+import { AppLoading } from 'expo';
+import { StatusBar } from 'react-native';
+
 import { Roboto_400Regular, Roboto_500Medium } from '@expo-google-fonts/roboto';
 import { Ubuntu_700Bold, useFonts } from '@expo-google-fonts/ubuntu';
-import { AppLoading } from 'expo';
 
 import Routes from './src/routes';
-
-
-
 
 export default function App() {
 
@@ -20,17 +18,27 @@ export default function App() {
   if(!fontsLoaded){
     return <AppLoading />
   }
-
-
+ 
   return (
-
-    <>
-      <StatusBar barStyle="dark-content" backgroundColor="transparent" translucent/>
-      <Routes />
-    </>
-    
+      <>
+        
+        <StatusBar barStyle="dark-content" backgroundColor="transparent" translucent={false} />     
+        <Routes />
+      </>
   );
 }
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
